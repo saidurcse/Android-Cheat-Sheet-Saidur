f3bcd4413bb1665d23f27613ace16c863ca9ef08
Saidur Rahman <saidurcse@users.noreply.github.com>
2021-08-06 13:13:35 +0600

    Update LowHighIndex.java

--- src/arrays/LowHighIndex.java
+++ src/arrays/LowHighIndex.java
@@ -2,22 +2,21 @@ import java.io.*;
 
 public class LowHighIndex {
 
-
     /*
      * Given a sorted array of integers, return the low and high index of the given key.
      * Return -1 if not found. The array length can be in millions with lots of duplicates.
      * */
-
-
+    
     /*
      * Linear search might be inefficient if the arr length exceeds a millions elements
      * so we can use a modified version of binary search
      * */
-
+    
     /*
      * Since we are using binary search the runtime complexity is Logarithmic, O(logn).
      * Memory Complexity = O(1)
      * */
+    
     public static void findLowestHighestIndex(int[] arr, int key) {
         int lowestIndex = arr.length-1;
         int highestIndex = 0;
@@ -35,12 +34,8 @@ public class LowHighIndex {
         System.out.println(lowestIndex + " " + highestIndex);
     }
 
-    
-
-
     public static void main(String[] args) {
         int[] arr = {1, 2, 5, 5, 5, 5, 5, 5, 5, 5, 20};
         findLowestHighestIndex(arr, 5);
     }
-
 }
