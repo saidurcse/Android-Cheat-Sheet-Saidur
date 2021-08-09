53b58f81af7a1c3687c46a4b93be13a68f5a486f
Saidur Rahman <saidurcse@users.noreply.github.com>
2021-08-09 22:04:50 +0600

    Update SmallestCommonNumber.java

--- src/arrays/SmallestCommonNumber.java
+++ src/arrays/SmallestCommonNumber.java
@@ -1,18 +1,21 @@
-package arrays;
+import java.io.*;
+import java.util.*;
 
 public class SmallestCommonNumber {
 
-
+    
     /*
-     * Given three integer arrays sorted in ascending order, f
-     * ind the smallest number that is common in all three arrays.
+     * Given three integer arrays sorted in ascending order, 
+     * find the smallest number that is common in all three arrays.
      * */
 
     /*
      * Take advantage of the sorted array to reduce complexity.
      * Runtime Complexity: Linear, O(n).
      * Memory Complexity: Constant, O(1)
+     * 
      * */
+	
     public static int findSmallestCommonNumber(int[] arr1,
                                                int[] arr2,
                                                int[] arr3) {
@@ -42,7 +45,6 @@ public class SmallestCommonNumber {
     }
 
 
-
     public static void main(String[] args) {
         int[] arr1 = {6, 7, 10, 25, 30, 63, 64};
         int[] arr2 = {-1, 4, 5, 6, 7, 8, 50};
@@ -50,3 +52,6 @@ public class SmallestCommonNumber {
         System.out.println(findSmallestCommonNumber(arr1, arr2, arr3));
     }
 }
+
+
+/* Output: 6 */
