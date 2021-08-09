958330b88249fad3b016422a57c5e3d460c65cf1
Saidur Rahman <saidurcse@users.noreply.github.com>
2021-08-09 13:23:58 +0600

    Update MergeOverlappingIntervals.java

--- src/arrays/MergeOverlappingIntervals.java
+++ src/arrays/MergeOverlappingIntervals.java
@@ -1,10 +1,9 @@
-package arrays;
-
+import java.io.*;
+import java.util.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MergeOverlappingIntervals {
-
 
     /*
      * Given an array of intervals as input where each interval has a start and end timestamps.
@@ -22,7 +21,6 @@ public class MergeOverlappingIntervals {
      * Memory Complexity - Constant O(1)
      * */
 
-
     /*
      * For each interval in the input list,
      * Step 1: check if the current element  value < the output interval y value. If it is, then it is overlapping
@@ -32,24 +30,22 @@ public class MergeOverlappingIntervals {
     public static List<Tuple<Integer, Integer>> mergeIntervals(List<Tuple<Integer, Integer>> v1) {
 
         List<Tuple<Integer, Integer>> v2 = new ArrayList<>();
-        v2.add(new Tuple<>(v1.get(0).x, v1.get(0).y));
+        v2.add(new Tuple<>(v1.get(0).x, v1.get(0).y)); // 10,12
 
         for(int i=1; i<v1.size(); i++) {
-            int x1 = v1.get(i).x;
-            int y1 = v1.get(i).y;
-            int x2 = v2.get(v2.size()-1).getX();
-            int y2 = v2.get(v2.size()-1).getY();
+            int x1 = v1.get(i).x; //11
+            int y1 = v1.get(i).y; // 15
+            int x2 = v2.get(v2.size()-1).getX(); // 10
+            int y2 = v2.get(v2.size()-1).getY(); // 12
 
             if(y2 >= x1) {
-                v2.get(v2.size()-1).setY(Math.max(y1,y2));
+                v2.get(v2.size()-1).setY(Math.max(y1,y2)); // 10,15
             } else {
                 v2.add(new Tuple<>(x1,y1));
             }
         }
         return v2;
     }
-
-
 
     public static void main(String[] args) {
         List<Tuple<Integer, Integer>> v1 = new ArrayList<>();
@@ -63,7 +59,6 @@ public class MergeOverlappingIntervals {
             System.out.println(tuple.x + " " + tuple.y);
         }
     }
-
 
     public static class Tuple<X, Y> {
         private X x;
@@ -90,3 +85,5 @@ public class MergeOverlappingIntervals {
         }
     }
 }
+
+/* Output: 10, 15 */
