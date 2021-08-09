e67e25e441450d7ad29d5aa29e8da4aa0e9ffd39
Saidur Rahman <saidurcse@users.noreply.github.com>
2021-08-09 22:27:17 +0600

    Update MinimumDistanceBetweenTwoNumbers.java

--- src/arrays/MinimumDistanceBetweenTwoNumbers.java
+++ src/arrays/MinimumDistanceBetweenTwoNumbers.java
@@ -1,22 +1,29 @@
-package arrays;
-
-
+import java.io.*;
+import java.util.*;
 
 public class MinimumDistanceBetweenTwoNumbers {
-
+    
+    /*
+	 * arr[] = {3, 5, 4, 2, 6, 5, 6, 6, 5, 4, 8, 3}, x = 3, y = 6 
+	 * Output: Minimum distance between 3 and 6 is 4. 
+	 * Explanation:3 is at index 0 and 6 is at index 5, 
+	 * so the distance is 4
+	 */
 
     public static int findMinimumDistance(int[] arr, int n, int x, int y) {
         int minDist = Integer.MAX_VALUE;
 
         for(int i=0; i<n; i++) {
-
             for(int j=i+1; j<n; j++) {
                 if((x == arr[i] && y == arr[j]) ||
                         (y == arr[i] && x == arr[j]) &&
-                                minDist < Math.abs(i-j)) {
+                                minDist > Math.abs(i-j)) {
                     minDist = Math.abs(i-j);
                 }
             }
+        }
+        if (minDist > n) {
+            return -1;
         }
         return minDist;
     }
@@ -29,3 +36,6 @@ public class MinimumDistanceBetweenTwoNumbers {
         System.out.println(findMinimumDistance(arr, n, x, y));
     }
 }
+
+
+/* Output: 4 */
