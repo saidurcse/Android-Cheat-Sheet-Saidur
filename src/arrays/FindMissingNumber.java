ba5c37fab30600bd9770d6b7eb9b544f8e2341a3
Saidur Rahman <saidurcse@users.noreply.github.com>
2021-08-10 15:49:05 +0600

    Update FindMissingNumber.java

--- src/arrays/FindMissingNumber.java
+++ src/arrays/FindMissingNumber.java
@@ -1,10 +1,10 @@
 package arrays;
 
 public class FindMissingNumber {
-
-    /*
+	/*
      * Find missing numbers from an array
      * */
+	
     public static int findMissingNumbers(int[] arr, int count) {
 
         int i, total;
@@ -15,9 +15,11 @@ public class FindMissingNumber {
         return total;
     }
 
-
     public static void main(String[] args) {
         int[] arr = {1, 2, 4,6, 3, 7, 8};
         System.out.println(findMissingNumbers(arr, arr.length));
     }
 }
+
+
+/* Output: 5 */
