b2cf7ade065fe43d113cd37fee8b75e04f5254da
Saidur Rahman <saidurcse@users.noreply.github.com>
2021-08-13 13:39:49 +0600

    Update QueuesUsingStack.java

--- src/queue/QueuesUsingStack.java
+++ src/queue/QueuesUsingStack.java
@@ -1,9 +1,8 @@
-package queue;
-
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
+import java.util.ArrayDeque;
 
 public class QueuesUsingStack {
-
 
     /*
      * Implement a queue using stack.
@@ -24,19 +23,18 @@ public class QueuesUsingStack {
      * */
 
     public static void main(String[] args) throws Exception {
+    	
         QueueUsingStackWithFasterEnqueue queueUsingStackWithFasterEnqueue = new QueueUsingStackWithFasterEnqueue();
-        queueUsingStackWithFasterEnqueue.enqueue(3);
-        queueUsingStackWithFasterEnqueue.enqueue(6);
-        queueUsingStackWithFasterEnqueue.enqueue(10);
-        queueUsingStackWithFasterEnqueue.dequeue();
+        queueUsingStackWithFasterEnqueue.enqueue(3); //q1 = 3
+        queueUsingStackWithFasterEnqueue.enqueue(6); //q1 = 6,3
+        queueUsingStackWithFasterEnqueue.enqueue(10); //q1 = 10,6,3
+        queueUsingStackWithFasterEnqueue.dequeue(); //q2 = 10,6    3 removed
         System.out.println(queueUsingStackWithFasterEnqueue.stack1 + "###" + queueUsingStackWithFasterEnqueue.stack2);
 
-
-        queueUsingStackWithFasterEnqueue.enqueue(8);
-        queueUsingStackWithFasterEnqueue.enqueue(4);
-        queueUsingStackWithFasterEnqueue.dequeue();
+        queueUsingStackWithFasterEnqueue.enqueue(8); //q1 = 8
+        queueUsingStackWithFasterEnqueue.enqueue(4); //q1 = 8,4
+        queueUsingStackWithFasterEnqueue.dequeue(); //q2 = 10
         System.out.println(queueUsingStackWithFasterEnqueue.stack1 + "###" + queueUsingStackWithFasterEnqueue.stack2);
-
 
         /*  Faster Dequeue  */
         QueueUsingStackWithFasterDequeue queueUsingStackWithFasterDequeue = new QueueUsingStackWithFasterDequeue();
@@ -46,29 +44,26 @@ public class QueuesUsingStack {
         System.out.println(queueUsingStackWithFasterDequeue.dequeue()); //q1 = 10,6     3 is removed
         System.out.println(queueUsingStackWithFasterDequeue.stack1 + "###" + queueUsingStackWithFasterDequeue.stack2);
 
-
         queueUsingStackWithFasterDequeue.enqueue(8);     //q1 = 8,10,6
-        queueUsingStackWithFasterDequeue.enqueue(4);     //q1 = 4, 8,10,6
+        queueUsingStackWithFasterDequeue.enqueue(4);     //q1 = 4,8,10,6
         System.out.println(queueUsingStackWithFasterDequeue.dequeue());          //q1 = 4,8,10      6 is removed
         System.out.println(queueUsingStackWithFasterDequeue.stack1 + "###" + queueUsingStackWithFasterDequeue.stack2);
-
     }
 }
 
-
-    /*
-     * enqueue(): Always push on stack1
-     * dequeue():
-     *      If queue size is 0, throw exception.
-     *      If stack2 has element(s), pop the topmost and return.
-     *      Otherwise If stack1 is non empty, pop all elements from stack1 and push them in stack2.
-     *      At the end pop stack2's top most element and return.
-     * */
+	/*
+	 * enqueue(): Always push on stack1
+	 * dequeue():
+	 *      If queue size is 0, throw exception.
+	 *      If stack2 has element(s), pop the topmost and return.
+	 *      Otherwise If stack1 is non empty, pop all elements from stack1 and push them in stack2.
+	 *      At the end pop stack2's top most element and return.
+	 * */
 
 class QueueUsingStackWithFasterEnqueue {
-        Stack<Integer> stack1 = new Stack<>();
-        Stack<Integer> stack2 = new Stack<>();
-
+	
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
 
     public void enqueue(int data) {
         stack1.push(data);
@@ -94,41 +89,51 @@ class QueueUsingStackWithFasterEnqueue {
 }
 
 
+	/*
+	 * enqueue():
+	 *      Pop each element from stack1 and push it to stack2.
+	 *      Push item being enqueued to stack1.
+	 *      Pop each element back from stack2 and push it to stack1.
+	 * dequeue():
+	 *      If queue size is 0, throw exception.
+	 *      Pop from stack1 and return.
+	 * */
 
-    /*
-     * enqueue():
-     *      Pop each element from stack1 and push it to stack2.
-     *      Push item being enqueued to stack1.
-     *      Pop each element back from stack2 and push it to stack1.
-     * dequeue():
-     *      If queue size is 0, throw exception.
-     *      Pop from stack1 and return.
-     * */
-    class QueueUsingStackWithFasterDequeue {
-        Stack<Integer> stack1 = new Stack<>();
-        Stack<Integer> stack2 = new Stack<>();
+class QueueUsingStackWithFasterDequeue {
+	
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
 
+    public boolean isEmpty() {
+        return stack1.size() + stack2.size() == 0;
+    }
 
-        public boolean isEmpty() {
-            return stack1.size() + stack2.size() == 0;
+    public void enqueue(int data) {
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
         }
 
-        public void enqueue(int data) {
-            while (!stack1.isEmpty()) {
-                stack2.push(stack1.pop());
-            }
-
-            stack1.push(data);
-            while (!stack2.isEmpty()) {
-                stack1.push(stack2.pop());
-            }
-        }
-
-        public int dequeue() throws Exception {
-            if(isEmpty()) {
-                throw new Exception("Stack is empty!");
-            }
-
-            return stack1.pop();
+        stack1.push(data);
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
         }
     }
+
+    public int dequeue() throws Exception {
+        if(isEmpty()) {
+            throw new Exception("Stack is empty!");
+        }
+
+        return stack1.pop();
+    }
+}
+
+
+/* Output: 
+ * []###[10, 6]
+ * [8, 4]###[10]
+ * 3
+ * [10, 6]###[]
+ * 6
+ * [4, 8, 10]###[] 
+ */
