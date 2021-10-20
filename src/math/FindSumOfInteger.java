1eeee8eb17d14b076687b4b4e1f747ecc8e4090e
Saidur Rahman <saidurcse@users.noreply.github.com>
2021-10-20 17:24:31 +0600

    Update FindSumOfInteger.java

--- src/math/FindSumOfInteger.java
+++ src/math/FindSumOfInteger.java
@@ -1,16 +1,18 @@
-package math;
+import java.io.*;
+import java.util.*;
+import javafx.util.Pair;
 
 public class FindSumOfInteger {
-
 
     public static int findSumOfDigits(int num) {
         return num == 0 ? 0 : (num%10) + findSumOfDigits(num/10) ;
     }
-
-
 
     public static void main(String[] args) {
         int n = 4321;
         System.out.println(findSumOfDigits(n));
     }
 }
+
+
+/* Output: 10 */
